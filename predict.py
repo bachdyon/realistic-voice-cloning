bde4562cbca2475233654ed8096a39905c71fc34
bachdyon <157558061+bachdyon@users.noreply.github.com>
2025-03-01 11:43:29 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -17,6 +17,7 @@ import main as m
 def download_online_model(url, dir_name):
     print(f"[~] Downloading voice model with name {dir_name}...")
     zip_name = url.split("/")[-1]
+    zip_name = zip_name.split('?')[0]
     extraction_folder = os.path.join(m.rvc_models_dir, dir_name)
     if os.path.exists(extraction_folder):
         print(f"Voice model directory {dir_name} already exists! Skipping download.")
